3fec5e66e50979280ae31ad85cb4973751c1d93f
Alzeros <82042404+Alzeros@users.noreply.github.com>
2026-08-20 17:48:09 +0800

    ci(release): 增加便携版构建上传

--- src/components/SettingsDialog.svelte
+++ src/components/SettingsDialog.svelte
@@ -80,7 +80,7 @@
     if (section === 'about' && !version.value) {
       getVersion()
         .then((v) => (version.value = v))
-        .catch(() => (version.value = '0.1.3'));
+        .catch(() => (version.value = '0.2.4'));
     }
   }
 
