1eb7e380109efc416f329ccae9d2074bd807458d
Alzeros <82042404+Alzeros@users.noreply.github.com>
2026-09-08 11:10:43 +0800

    chore(release): 发布 v0.3.2

--- src/components/SettingsDialog.svelte
+++ src/components/SettingsDialog.svelte
@@ -189,7 +189,7 @@
     if (section === 'about' && !version.value) {
       getVersion()
         .then((v) => (version.value = v))
-        .catch(() => (version.value = '0.3.1'));
+        .catch(() => (version.value = '0.3.2'));
     }
   }
 
